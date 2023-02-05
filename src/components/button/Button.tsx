41ab960fcd42a8806f90f318336c9fc4f57b3c2d
Sergei <samkorbut@gmail.com>
2023-02-05 10:24:17 +0400

    Add Button.tsx

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,7 +1,18 @@
+import {MouseEventHandler} from "react";
+import style from 'Button.module.css'
 
+type ButtonPropsType = {
+    title: string
+    onClick: MouseEventHandler<HTMLButtonElement>
+    disabled: boolean | undefined
+}
 
-export const Button =()=> {
+export const Button = (props: ButtonPropsType) => {
     return (
-        <button>title</button>
+        <button className={props.disabled ? style.disabled : style.button}
+                onClick={props.onClick}
+                disabled={props.disabled}>
+            {props.title}
+        </button>
     )
 }
